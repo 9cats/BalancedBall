c0e0db6029f8f6a038cafa9dd883f7dca806886d
HuangYi <123337671@qq.com>
2022-06-22 15:53:37 +0800

    :sparkles: 添加 LCD

--- BSP/bsp.c
+++ BSP/bsp.c
@@ -1,9 +1,13 @@
 
 #include "W25Q64/qspi_w25q64.h"
 #include "SDRAM/sdram.h"
+#include "LCD/lcd_rgb.h"
+#include "LCD/touch.h"
 
 void BSP_Init(void)
 {
 	QSPI_W25Qxx_Init();
 	MX_FMC_Init();
+	MX_LTDC_Init();
+	Touch_Init();
 }
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -24,8 +24,10 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include "../../BSP/bsp.h"
+#include "../../BSP/LCD/lcd_rgb.h"
+#include "../../BSP/LCD/touch.h"
 #include "../../BSP/W25Q64/qspi_w25q64.h"
+#include "../../BSP/bsp.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -103,11 +105,24 @@ int main(void)
   MX_GPIO_Init();
   MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
-  /* FMC_NE1 : 0x6000 0000 ~ 0x6400 0000 */
-  volatile uint32_t data = 0;
-         *(uint32_t *)0x60000000 = 0x12345678;
-  data = *(uint32_t *)0x60000000;
-  (void)data;
+	LCD_SetColor(0xff333333);					//	设置画笔色，使用自定义颜色
+	LCD_SetBackColor(0xffB9EDF8); 			//	设置背景色，使用自定义颜色
+	LCD_Clear(); 									//	清屏，刷背景色
+
+	LCD_SetTextFont(&CH_Font32);
+	LCD_DisplayText( 42, 20,"电容触摸测试");
+	LCD_DisplayText( 42, 70,"核心板型号：FK743M1-IIT6");
+	LCD_DisplayText( 42, 120,"屏幕分辨率：800*480");
+
+
+
+	LCD_DisplayString(44, 170,"X1:       Y1:");
+	LCD_DisplayString(44, 220,"X2:       Y2:");
+	LCD_DisplayString(44, 270,"X3:       Y3:");
+	LCD_DisplayString(44, 320,"X4:       Y4:");
+	LCD_DisplayString(44, 370,"X5:       Y5:");
+
+	LCD_SetColor(LCD_RED);	//设置画笔颜色
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -117,8 +132,27 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-    HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
-    HAL_Delay(1000);
+		Touch_Scan();	// 触摸扫描
+
+		if(touchInfo.flag == 1)
+		{
+		   LCD_DisplayNumber(110,170,touchInfo.x[0],4);	// 显示第1组坐标
+			LCD_DisplayNumber(260,170,touchInfo.y[0],4);
+
+			LCD_DisplayNumber(110,220,touchInfo.x[1],4);	// 显示第2组坐标
+			LCD_DisplayNumber(260,220,touchInfo.y[1],4);
+
+			LCD_DisplayNumber(110,270,touchInfo.x[2],4);	// 显示第3组坐标
+			LCD_DisplayNumber(260,270,touchInfo.y[2],4);
+
+			LCD_DisplayNumber(110,320,touchInfo.x[3],4);	// 显示第4组坐标
+			LCD_DisplayNumber(260,320,touchInfo.y[3],4);
+
+			LCD_DisplayNumber(110,370,touchInfo.x[4],4);	// 显示第5组坐标
+			LCD_DisplayNumber(260,370,touchInfo.y[4],4);
+		}
+		HAL_Delay(20);		// GT911触摸屏扫描间隔不能小于10ms，建议设置为20ms
+
   }
   /* USER CODE END 3 */
 }
@@ -186,21 +220,19 @@ void SystemClock_Config_Fix(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
 
   /** Supply configuration update enable
   */
   HAL_PWREx_ConfigSupply(PWR_LDO_SUPPLY);
-
   /** Configure the main internal regulator output voltage
   */
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE0);
 
   while(!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {}
-
   /** Macro to configure the PLL clock source
   */
   __HAL_RCC_PLL_PLLSOURCE_CONFIG(RCC_PLLSOURCE_HSE);
-
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
@@ -211,7 +243,7 @@ void SystemClock_Config_Fix(void)
   RCC_OscInitStruct.PLL.PLLM = 5;
   RCC_OscInitStruct.PLL.PLLN = 192;
   RCC_OscInitStruct.PLL.PLLP = 2;
-  RCC_OscInitStruct.PLL.PLLQ = 8;
+  RCC_OscInitStruct.PLL.PLLQ = 2;
   RCC_OscInitStruct.PLL.PLLR = 2;
   RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1VCIRANGE_2;
   RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1VCOWIDE;
@@ -220,21 +252,45 @@ void SystemClock_Config_Fix(void)
   {
     Error_Handler();
   }
-
   /** Initializes the CPU, AHB and APB buses clocks
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2
                               |RCC_CLOCKTYPE_D3PCLK1|RCC_CLOCKTYPE_D1PCLK1;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.SYSCLKDivider = RCC_SYSCLK_DIV2;
+  RCC_ClkInitStruct.SYSCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV2;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV1;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV2;
   RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV2;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /* 设置LTDC时钟，这里设置为33MHz，即刷新率在60帧左右，过高或者过低都会造成闪烁 */
+  /* LCD clock configuration */
+  /* PLL3_VCO Input = HSE_VALUE/PLL3M = 1 Mhz */
+  /* PLL3_VCO Output = PLL3_VCO Input * PLL3N = 330 Mhz */
+  /* PLLLCDCLK = PLL3_VCO Output/PLL3R = 330/10 = 33 Mhz */
+  /* LTDC clock frequency = PLLLCDCLK = 33 Mhz */
+
+
+  PeriphClkInitStruct.PLL3.PLL3M = 25;
+  PeriphClkInitStruct.PLL3.PLL3N = 330;
+  PeriphClkInitStruct.PLL3.PLL3P = 2;
+  PeriphClkInitStruct.PLL3.PLL3Q = 2;
+  PeriphClkInitStruct.PLL3.PLL3R = 10;
+  PeriphClkInitStruct.PLL3.PLL3RGE = RCC_PLL3VCIRANGE_0;
+  PeriphClkInitStruct.PLL3.PLL3VCOSEL = RCC_PLL3VCOMEDIUM;
+  PeriphClkInitStruct.PLL3.PLL3FRACN = 0;
+
+  PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_LTDC|RCC_PERIPHCLK_USART1|RCC_PERIPHCLK_FMC;
+  PeriphClkInitStruct.FmcClockSelection = RCC_FMCCLKSOURCE_D1HCLK;
+  PeriphClkInitStruct.Usart16ClockSelection = RCC_USART16CLKSOURCE_D2PCLK2;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
   {
     Error_Handler();
   }
